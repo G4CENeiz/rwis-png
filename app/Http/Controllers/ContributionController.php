4da4dd80660d6a6461ddedd58f7158e1e3844d39
Baihaqi <baihaqilearning+github@gmail.com>
2024-06-07 21:58:07 +0700

    feat: work on contribution index

--- app/Http/Controllers/ContributionController.php
+++ app/Http/Controllers/ContributionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Contribution;
 use App\Http\Requests\StoreContributionRequest;
 use App\Http\Requests\UpdateContributionRequest;
+use Illuminate\Http\Request;
 
 class ContributionController extends Controller
 {
@@ -14,6 +15,50 @@ class ContributionController extends Controller
     public function index()
     {
         //
+        $breadcrumb = (object) [
+            'title' => 'Daftar Iuran',
+            'list' => [
+                [
+                    'item'  => 'Administrasi',
+                    'route' => 'administration.ledger.index'
+                ],
+                [
+                    'item'  => 'Daftar Iuran',
+                    'route' => 'administration.contribution.index'
+                ],
+            ],
+        ];
+        $card = (object) [
+            'title' => 'Daftar Iuran yang ditagihkan'
+        ];
+        $page = [
+            'title' => 'Daftar Iuran'
+        ];
+        return view(
+            'administration.contribution.contribution.index',
+            [
+                'breadcrumb' => $breadcrumb,
+                'card' => $card,
+                'page' => $page,
+            ]
+        );
+    }
+
+    public function list(Request $request) {
+        $contributions = Contribution::all()->with('level')->get();
+        if ($request->level_id) {
+            $contributions = $contributions->where('level_id', $request->level_id);
+        }
+        // return DataTables::of($contributions)->addIndexColumn()->addColumn('action', function ($user) {
+        //         $btn = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn-sm">Detail</a> ';
+        //         $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
+        //         $btn .= '<form class="d-inline-block" method="POST" action="' . url('/user/'.$user->user_id) . '">'
+        //         . csrf_field()
+        //         . method_field('DELETE')
+        //         . '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Are you sure to delete this data?\');">Delete</button>' 
+        //         . '</form>';
+        //         return $btn;
+        //     })->rawColumns(['action'])->make(true);
     }
 
     /**
--- resources/views/administration/contribution/contribution/index.blade.php
+++ resources/views/administration/contribution/contribution/index.blade.php
@@ -1,3 +1,41 @@
-<div>
-    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
+@extends('layouts.vertical')
+
+
+@section('css')
+@endsection
+
+@section('content')
+<div class="card card-outline card-primary card-body">
+    <div class="list-group-item d-flex justify-content-between">
+        <h3 class="card-title">{{ $card->title }}</h3>
+        <div class="card-tools">
+            <a class="btn btn-sm btn-primary mt-1" href="{{ route('administration.contribution.create') }}">Tambah</a>
+        </div>
+    </div>
+    <div class="list-group-item">
+        @if (session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
+        @if (session('error'))
+            <div class="alert alert-danger">{{ session('error') }}</div>
+        @endif
+        <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Nama</th>
+                    <th>Level Pengguna</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
 </div>
+@endsection
+
+@section('script')
+@endsection
+
+@section('script-bottom')
+@endsection
