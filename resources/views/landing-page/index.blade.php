aa2ef1416687e7d7c0828c6ff210c3e213b8d96d
Mochamad Wildani Azizi <wwilldanazizi@gmail.com>
2024-06-10 23:05:13 +0700

    fixing our features section

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -14,36 +14,7 @@
         border-radius: 0px; /* Menambahkan sudut yang melengkung */
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Menambahkan bayangan */
     }
-    .gradient-text {
-        /* Set the font size (adjust as needed) */
-        font-size: 24px;
-
-        /* Apply the linear gradient */
-        background-image: linear-gradient(45deg, #2e382d, #42af42);
-
-        /* Make the gradient cover the full width and height of the text field */
-        background-size: 100%;
-        
-        /* Clip the background to the text (important for gradient effect) */
-        -webkit-background-clip: text;
-        background-clip: text;
-
-        /* Set the text color to transparent (so the gradient shows through) */
-        color: transparent;
-    }
 </style>
-@endsection
-
-@section('breadcrumb')
-<div class="row page-title align-items-center">
-    <nav aria-label="breadcrumb" class="float-right mt-1">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="#">Shreyu</a></li>
-            <li class="breadcrumb-item"><a href="#">Pages</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Starter</li>
-        </ol>
-    </nav>
-</div>
 @endsection
 
 @section('content')
@@ -73,128 +44,87 @@
         </div>
     </div>
 </div>
-<div class="row page-title align-items-center">
-    <div>
-        <button type="button" class="btn btn-outline-secondary btn-rounded" data-toggle="tooltip" data-placement="center" title="" style="margin: 0; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); padding-top:1%;">
-            Version 1.0 is here
-        </button>        
-    </div>
-    <div>
-        <div class="row no-gutters align-items-center" style="width: 70%; display: block; margin-left: auto; margin-right: auto; filter: drop-shadow(8px 8px 16px rgba(0, 0, 0, 0.5)); padding-top:5%;">
-            <img src="{{ URL::asset('assets/images/dashboard.png') }}" class="card-img" alt="pc-rwis">
-        </div>
-    </div>
-
-    <div class="row" style="padding: 5%">
+<div class="row align-items-center col-md-12 col-xl-12">
+    <div class="row" style="padding-top: 1%; padding-left: 5%; padding-right: 5%; text-align: center">
+        <div style="text-align:center"><h1>Our Features</h1></div>
         <div class="col-xl-12">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="header-title mb-4 mt-0">Our Features</h5>
                     <ul class="nav nav-pills navtab-bg nav-justified">
+                        <li class="nav-item">
+                            <a href="#home1" data-toggle="tab" aria-expanded="true"
+                                class="nav-link active">
+                                <span class="d-block d-sm-none"><i class="uil-home"></i></span>
+                                <span class="d-none d-sm-block">Dashboard</span>
+                            </a>
+                        </li>
                         <li class="nav-item">
                             <a href="#adm1" data-toggle="tab" aria-expanded="false"
                                 class="nav-link">
-                                <span class="d-block d-sm-none"><i class="uil-home-alt"></i></span>
+                                <span class="d-block d-sm-none"><i class="uil-administrasi"></i></span>
                                 <span class="d-none d-sm-block">Administrasi</span>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#profile1" data-toggle="tab" aria-expanded="false"
-                                class="nav-link active">
-                                <span class="d-block d-sm-none"><i class="uil-user"></i></span>
-                                <span class="d-none d-sm-block">Profile</span>
+                            <a href="#info1" data-toggle="tab" aria-expanded="false"
+                                class="nav-link">
+                                <span class="d-block d-sm-none"><i class="uil-info"></i></span>
+                                <span class="d-none d-sm-block">Informasi Warga</span>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#messages1" data-toggle="tab" aria-expanded="true"
+                            <a href="#bansos1" data-toggle="tab" aria-expanded="false"
                                 class="nav-link">
-                                <span class="d-block d-sm-none"><i class="uil-envelope"></i></span>
-                                <span class="d-none d-sm-block">Messages</span>
+                                <span class="d-block d-sm-none"><i class="uil-bansos"></i></span>
+                                <span class="d-none d-sm-block">Bansos</span>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#messages1" data-toggle="tab" aria-expanded="false"
+                            <a href="#umkm1" data-toggle="tab" aria-expanded="false"
                                 class="nav-link">
-                                <span class="d-block d-sm-none"><i class="uil-envelope"></i></span>
-                                <span class="d-none d-sm-block">Messages</span>
+                                <span class="d-block d-sm-none"><i class="uil-umkm"></i></span>
+                                <span class="d-none d-sm-block">UMKM</span>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#messages1" data-toggle="tab" aria-expanded="false"
+                            <a href="#artikel1" data-toggle="tab" aria-expanded="false"
                                 class="nav-link">
-                                <span class="d-block d-sm-none"><i class="uil-envelope"></i></span>
-                                <span class="d-none d-sm-block">Messages</span>
+                                <span class="d-block d-sm-none"><i class="uil-artikel"></i></span>
+                                <span class="d-none d-sm-block">Artikel</span>
                             </a>
                         </li>
                     </ul>
                     <div class="tab-content text-muted">
+                        <div class="tab-pane show active" id="home1">
+                            <div>
+                                <div class="row no-gutters align-items-center" style="width: 70%; display: block; margin-left: auto; margin-right: auto; filter: drop-shadow(4px 4px 8px rgba(0, 0, 0, 0.5)); padding-top:1.5%;">
+                                    <img src="{{ URL::asset('assets/images/dashboard.png') }}" class="card-img" alt="pc-rwis">
+                                </div>
+                            </div>
+                            <p style="padding-top: 1.5%">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident veniam, perferendis temporibus soluta porro accusantium quidem facilis fuga deserunt harum omnis quo eligendi quaerat, architecto quisquam facere. Voluptate pariatur atque vitae sit facere aspernatur nesciunt, natus inventore tenetur illum dignissimos ducimus, veritatis ipsa iusto illo consequatur recusandae! Aliquam deserunt aliquid nisi sit aperiam, labore fugit quo ducimus ex cupiditate laudantium in perspiciatis quisquam neque officiis deleniti laborum fugiat ad libero rerum. Et nihil repellat soluta provident impedit error consectetur possimus voluptatibus labore dicta, tenetur dolorem autem, quaerat cupiditate quis praesentium, animi dignissimos ullam maiores a eaque iusto eos. Obcaecati, ducimus?</p>
+                        </div>
+
                         <div class="tab-pane" id="adm1">
-                            <p>Mudahkan urusan administratif dan akses data penting di RW Anda</p>
+                            <div>
+                                
+                            </div>
+                            <p style="padding-top: 1.5%">Mudahkan urusan administratif dan akses data penting di RW Anda. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor quidem vel deleniti. Culpa incidunt eveniet doloribus. Pariatur laborum dolores cum, voluptatibus voluptate sequi veritatis labore cumque in unde, repellendus eveniet.</p>
                         </div>
-                        <div class="tab-pane" id="profile1">
-                            <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-                                In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-                                Nullam dictum felis eu pede mollis pretium. Integer tincidunt.Cras
-                                dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
-                                tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend
-                                ac, enim.</p>
-                            <p class="mb-0">Vakal text here dolor sit amet, consectetuer adipiscing
-                                elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                                natoque penatibus et magnis dis parturient montes, nascetur
-                                ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
-                                pretium quis, sem. Nulla consequat massa quis enim.</p>
+
+                        <div class="tab-pane" id="info1">
+                            <p style="padding-top: 1.5%">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos animi veritatis, nesciunt suscipit sapiente molestiae maiores, aut, fugit laborum consequatur deserunt laboriosam laudantium tempore sunt reiciendis. Quo hic laborum quod optio cum voluptas atque eius ratione deleniti perspiciatis iusto ipsum facere doloremque facilis, debitis saepe, vitae architecto nulla aliquam iure!</p>
                         </div>
-                        <div class="tab-pane show active" id="messages1">
-                            <p>Vakal text here dolor sit amet, consectetuer adipiscing elit. Aenean
-                                commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-                                penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                                Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-                                Nulla consequat massa quis enim.</p>
-                            <p class="mb-0">Donec pede justo, fringilla vel, aliquet nec, vulputate
-                                eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae,
-                                justo. Nullam dictum felis eu pede mollis pretium. Integer
-                                tincidunt.Cras dapibus. Vivamus elementum semper nisi. Aenean
-                                vulputate eleifend tellus. Aenean leo ligula, porttitor eu,
-                                consequat vitae, eleifend ac, enim.</p>
+
+                        <div class="tab-pane" id="bansos1">
+                            <p style="padding-top: 1.5%">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias laudantium perspiciatis fuga, voluptas suscipit est ea quae. Officiis totam nobis iusto earum, fuga vero? Ipsa ex deleniti nostrum vel eaque, cumque quia earum mollitia vitae beatae eligendi molestiae quam, repellendus voluptas accusamus itaque perferendis necessitatibus. Reprehenderit, sed laborum aliquam facilis ex numquam sint, quasi delectus reiciendis iure voluptate aperiam exercitationem quas explicabo dolor molestiae quos sequi quisquam tenetur eligendi et sit cupiditate illum! Sed molestiae ex, possimus nisi obcaecati dolor quisquam error ratione, corrupti nihil exercitationem molestias voluptas itaque omnis eaque temporibus adipisci. Ratione deserunt aspernatur ullam facilis repellat enim, nulla, blanditiis ut perspiciatis dolores provident ea unde natus sint cupiditate fugiat tenetur.</p>
                         </div>
-                        <div class="tab-pane" id="profile1">
-                            <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-                                In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-                                Nullam dictum felis eu pede mollis pretium. Integer tincidunt.Cras
-                                dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
-                                tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend
-                                ac, enim.</p>
-                            <p class="mb-0">Vakal text here dolor sit amet, consectetuer adipiscing
-                                elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                                natoque penatibus et magnis dis parturient montes, nascetur
-                                ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
-                                pretium quis, sem. Nulla consequat massa quis enim.</p>
+
+                        <div class="tab-pane" id="umkm1">
+                            <p style="padding-top: 1.5%">Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.</p>
                         </div>
-                        <div class="tab-pane" id="profile1">
-                            <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-                                In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-                                Nullam dictum felis eu pede mollis pretium. Integer tincidunt.Cras
-                                dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
-                                tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend
-                                ac, enim.</p>
-                            <p class="mb-0">Vakal text here dolor sit amet, consectetuer adipiscing
-                                elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                                natoque penatibus et magnis dis parturient montes, nascetur
-                                ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
-                                pretium quis, sem. Nulla consequat massa quis enim.</p>
-                        </div>
-                        <div class="tab-pane" id="profile1">
-                            <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-                                In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-                                Nullam dictum felis eu pede mollis pretium. Integer tincidunt.Cras
-                                dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
-                                tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend
-                                ac, enim.</p>
-                            <p class="mb-0">Vakal text here dolor sit amet, consectetuer adipiscing
-                                elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                                natoque penatibus et magnis dis parturient montes, nascetur
-                                ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
-                                pretium quis, sem. Nulla consequat massa quis enim.</p>
+
+                        <div class="tab-pane" id="artikel1">
+                            <p style="padding-top: 1.5%">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima voluptatum maxime id cupiditate velit sit ratione eligendi quia mollitia soluta est ab distinctio veniam totam molestias officiis fugiat facilis nobis, expedita eum tempore harum perspiciatis sint fuga. Ab soluta quam corporis! Quibusdam fugiat, temporibus nulla eos voluptatem illo nostrum ratione necessitatibus sit, ipsa distinctio tenetur. Necessitatibus eos facere commodi, amet ipsum doloribus adipisci eum, placeat asperiores labore harum totam exercitationem in. Sint voluptas magni dolor dolorum labore vel voluptatibus sunt dignissimos saepe veniam quod vitae aut, debitis voluptates ipsa similique ratione quam repudiandae reiciendis nisi asperiores nobis. Autem exercitationem ab vitae quidem incidunt est quibusdam quisquam nemo impedit harum distinctio nesciunt, eligendi doloribus in explicabo mollitia natus nam accusamus quae ea eius quis necessitatibus odit ut! Blanditiis consectetur similique nesciunt maxime qui voluptas quae perspiciatis culpa nisi vero mollitia quod repellat accusantium reprehenderit amet adipisci dolore porro possimus, hic debitis.</p>
                         </div>
                     </div>
                 </div>
@@ -226,13 +156,11 @@
 @endsection
 
 @section('script')
-<!-- optional plugins -->
-<script src="{{ URL::asset('assets/libs/moment/moment.min.js') }}"></script>
-<script src="{{ URL::asset('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
-<script src="{{ URL::asset('assets/libs/flatpickr/flatpickr.min.js') }}"></script>
+<!-- datatable js -->
+<script src="{{ URL::asset('assets/libs/datatables/datatables.min.js') }}"></script>
 @endsection
 
 @section('script-bottom')
-<!-- init js -->
-<script src="{{ URL::asset('assets/js/pages/dashboard.init.js') }}"></script>
+<!-- Datatables init -->
+<script src="{{ URL::asset('assets/js/pages/datatables.init.js') }}"></script>
 @endsection
