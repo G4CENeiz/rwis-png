3439e47398eb916386b97bc6e16d9e29c6199f23
Mochamad Wildani Azizi <wwilldanazizi@gmail.com>
2024-06-11 02:42:29 +0700

    Update header-landing.blade.php

--- resources/views/layouts/shared/header-landing.blade.php
+++ resources/views/layouts/shared/header-landing.blade.php
@@ -11,35 +11,76 @@
         </div>
         <div>
             {{-- DASHBOARD --}}
-            <a href="/" class="col-xl-3">
-                <span data-toggle="tooltip" data-original-title="Halaman Utama">Dashboard</span>
-            </a>
+            <div class="btn-group col-cl-3">
+                <a href="/" class="col-xl-3">
+                    <button type="button" class="btn btn-outline-dark width-md">
+                        Dashboard
+                    </button>
+                </a>
+            </div><!-- /btn-group -->
+
             {{-- ADMINISTRASI --}}
-            <a href="{{route('administration.ledger.index')}}" class="col-xl-3">
-                <span data-toggle="tooltip" data-original-title="Bantuan Sosial">Administrasi</span>
-            </a>
+            <div class="btn-group col-cl-3">
+                <div type="button" class="btn btn-outline-dark dropdown-toggle width-md"
+                    data-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">Administrasi<i class="icon"><span data-feather="chevron-down"></span></i></div>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Buku Kas</a>
+                    <a class="dropdown-item" href="#">Iuran</a>
+                    <a class="dropdown-item" href="#">Detail Iuran</a>
+                    <a class="dropdown-item" href="#">Tipe Iuran</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Pembayaran</a>
+                    <a class="dropdown-item" href="#">Metode Pembayaran</a>
+                    <a class="dropdown-item" href="#">Status Pembayaran</a>
+                    <a class="dropdown-item" href="#">Bukti Pembayaran</a>
+                </div>
+            </div><!-- /btn-group -->
+
             {{-- INFORMASI WARGA --}}
-            <a href="{{route('resident.information.index')}}" class="col-xl-3">
-                <span data-toggle="tooltip" data-original-title="Bantuan Sosial">Informasi Warga</span>
-            </a>
+            <div class="btn-group col-cl-3">
+                <a href="{{route('resident.information.index')}}" class="col-xl-3">
+                    <button type="button" class="btn btn-outline-dark width-md">
+                        Informasi Warga
+                    </button>
+                </a>
+            </div><!-- /btn-group -->
+
             {{-- BANSOS --}}
-            <a href="{{route('bansos.index')}}" class="col-xl-3">
-                <span data-toggle="tooltip" data-original-title="Bantuan Sosial">Bansos</span>
-            </a>
+            <div class="btn-group col-cl-3">
+                <a href="{{route('bansos.index')}}" class="col-xl-3">
+                    <button type="button" class="btn btn-outline-dark width-md">
+                        Bansos
+                    </button>
+                </a>
+            </div><!-- /btn-group -->
+            
             {{-- UMKM --}}
-            <a href="{{route('umkm.index')}}" class="col-xl-3">
-                <span data-toggle="tooltip" data-original-title="Usaha Masyarakat Kecil Menengah">UMKM</span>
-            </a>
+            <div class="btn-group col-cl-3">
+                <a href="{{route('umkm.index')}}" class="col-xl-3">
+                    <button type="button" class="btn btn-outline-dark width-md">
+                        UMKM
+                    </button>
+                </a>
+            </div><!-- /btn-group -->
+            
             {{-- MEDIA --}}
-            <a href="{{route('media.index')}}" class="success col-xl-3">
-                <span data-toggle="tooltip" data-original-title="Artikel">Artikel</span>
-            </a>
-            <a href="{{route('auth.login')}}" class="success col-xl-3">
-                <button type="button" class="btn btn-success" data-toggle="tooltip"
-                data-placement="bottom" title="" data-original-title="Login">
-                Masuk
-                </button>
-            </a>
+            <div class="btn-group col-cl-3">
+                <a href="{{route('media.index')}}" class="success col-xl-3">
+                    <button type="button" class="btn btn-outline-dark width-md">
+                        Artikel
+                    </button>
+                </a>
+            </div><!-- /btn-group -->
+
+            {{-- LOGIN --}}
+            <div class="btn-group col-cl-3">
+                <a href="{{route('auth.login')}}" class="success col-xl-3">
+                    <button type="button" class="btn btn-success width-md">
+                    Masuk
+                    </button>
+                </a>
+            </div><!-- /btn-group -->
         </div>           
     </div>
 </div>
