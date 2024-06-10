f4d59fdf84e8f2fb74b8f9d71893fec016eb162f
Baihaqi <baihaqilearning+github@gmail.com>
2024-06-10 21:34:36 +0700

    feat: add crud payment method

--- app/Http/Controllers/PaymentMethodController.php
+++ app/Http/Controllers/PaymentMethodController.php
@@ -14,6 +14,33 @@ class PaymentMethodController extends Controller
     public function index()
     {
         //
+        $breadcrumb = (object) [
+            'title' => 'Daftar Iuran',
+            'list' => [
+                [
+                    'item'  => 'Administrasi',
+                    'route' => 'administration.ledger.index'
+                ],
+                [
+                    'item'  => 'Daftar Iuran',
+                    'route' => 'administration.contribution.index'
+                ],
+            ],
+        ];
+        $card = (object) [
+            'title' => 'Daftar Iuran yang ditagihkan'
+        ];
+        $page = [
+            'title' => 'Daftar Iuran'
+        ];
+        return view(
+            'administration.contribution.payment-method.index',
+            [
+                'breadcrumb' => $breadcrumb,
+                'card' => $card,
+                'page' => $page,
+            ]
+        );
     }
 
     /**
@@ -22,6 +49,38 @@ class PaymentMethodController extends Controller
     public function create()
     {
         //
+        $breadcrumb = (object) [
+            'title' => 'Tambah Buku Kas',
+            'list' => [
+                [
+                    'item'  => 'Administrasi',
+                    'route' => 'administration.ledger.index'
+                ],
+                [
+                    'item'  => 'Buku Kas',
+                    'route' => 'administration.ledger.index'
+                ],
+                [
+                    'item'  => 'Tambah Buku Kas',
+                    'route' => 'administration.ledger.create'
+                ],
+            ],
+        ];
+        $card = (object) [
+            'title' => 'Tambah Buku Kas'
+        ];
+        $page = [
+            'title' => 'Tambah Buku Kas'
+        ];
+
+        return view(
+            'administration.contribution.general-ledger.create', 
+            [
+                'breadcrumb' => $breadcrumb,
+                'card' => $card,
+                'page' => $page,
+            ]
+        );
     }
 
     /**
@@ -30,12 +89,23 @@ class PaymentMethodController extends Controller
     public function store(StorePaymentMethodRequest $request)
     {
         //
+        $validated = $request->validated();
+
+        $validated = $request->safe()->only(['issuer_type','issuer_id']);
+        $validated = $request->safe()->except(['issuer_type','issuer_id']);
+
+        PaymentMethod::create([
+            'issuer_id' => $validated['issuer_id'],
+            'issuer_type' => $validated['issuer_type'],
+        ]);
+
+        return redirect('/administration/ledger');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(PaymentMethod $paymentMethod)
+    public function show(string $id)
     {
         //
     }
@@ -43,24 +113,80 @@ class PaymentMethodController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(PaymentMethod $paymentMethod)
+    public function edit(string $id)
     {
         //
+        $generalLedger = PaymentMethod::find($id);
+        $breadcrumb = (object) [
+            'title' => 'Ubah Buku Kas',
+            'list' => [
+                [
+                    'item'  => 'Administrasi',
+                    'route' => 'administration.ledger.index'
+                ],
+                [
+                    'item'  => 'Buku Kas',
+                    'route' => 'administration.ledger.index'
+                ],
+                [
+                    'item'  => 'Ubah Buku Kas',
+                    'route' => 'administration.ledger.create'
+                ],
+            ],
+        ];
+        $card = (object) [
+            'title' => 'Ubah Buku Kas'
+        ];
+        $page = [
+            'title' => 'Ubah Buku Kas'
+        ];
+
+        return view(
+            'administration.contribution.general-ledger.edit', 
+            [
+                'breadcrumb' => $breadcrumb,
+                'card' => $card,
+                'page' => $page,
+                'generalLedger' => $generalLedger,
+            ]
+        );
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePaymentMethodRequest $request, PaymentMethod $paymentMethod)
+    public function update(UpdatePaymentMethodRequest $request, string $id)
     {
         //
+        $validated = $request->validated();
+
+        $validated = $request->safe()->only(['issuer_type','issuer_id']);
+        $validated = $request->safe()->except(['issuer_type','issuer_id']);
+
+        PaymentMethod::find($id)->update([
+            'issuer_id' => $validated['issuer_id'],
+            'issuer_type' => $validated['issuer_type'],
+        ]);
+
+
+        return redirect('/administration/ledger');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(PaymentMethod $paymentMethod)
+    public function destroy(string $id)
     {
         //
+        $check = PaymentMethod::find($id);
+        if (!$check) {
+            return redirect('/administration/ledger')->with('error', 'Data Metode Pembayaran tidak ditemukan');
+        }
+        try {
+            PaymentMethod::find($id)->update(['is_archived' => true]);
+            return redirect('/administration/ledger')->with('success', 'Data Metode Pembayaran berhasil dihapus');
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect('/administration/ledger')->with('error', 'Data Metode Pembayaran gagal dihapus karena masih terdapat label lain yang terkait dengan data ini');
+        }
     }
 }
--- resources/views/administration/contribution/payment-method/create.blade.php
+++ resources/views/administration/contribution/payment-method/create.blade.php
@@ -1,3 +1,66 @@
-<div>
-    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
+@extends('layouts.vertical')
+
+
+@section('css')
+@endsection
+
+@section('content')
+<div class="card card-outline card-primary card-body">
+    <div class="list-group-item d-flex justify-content-between">
+        <h3 class="card-title">{{ $card->title }}</h3>
+        <div class="card-tools">
+            <a class="btn btn-sm btn-primary mt-1" href="{{ route('administration.payement.method.index') }}">Kembali</a>
+        </div>
+    </div>
+    <div class="list-group-item">
+        @if (session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
+        @if (session('error'))
+            <div class="alert alert-danger">{{ session('error') }}</div>
+        @endif
+        <form method="POST" action="{{ url('administration/payement/method') }}" class="form-horizontal">
+            @csrf
+            <div class="form-group row">
+                <label class="col-1 control-label col-form-label">Kepemilikan Buku Kas</label>
+                <div class="col-11">
+                    <select class="form-control" id="issuer_id" name="issuer_id" required>
+                        <option value="">- Pilih Level -</option>
+                        @foreach ($user as $item)
+                            <option value="{{ $item->user_id }}">{{ $item->name }}</option>
+                        @endforeach
+                    </select>
+                    @error('issuer_id')
+                        <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-1 control-label col-form-label">Tipe Buku Kas</label>
+                <div class="col-11">
+                    <select class="form-control" id="issuer_type" name="issuer_type" required>
+                        <option value="">- Pilih Level -</option>
+                        <option value="RT">RT</option>
+                        <option value="RW">RW</option>
+                    </select>
+                    @error('issuer_type')
+                        <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-1 control-label col-form-label"></label>
+                <div class="col-11">
+                    <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
+@endsection
+
+@section('script')
+@endsection
+
+@section('script-bottom')
+@endsection
