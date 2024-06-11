12804a29df93524bcbbcceb9ecdaf9a0140ecb06
Malik Abdul Azis <mhankazis@gmail.com>
2024-06-11 11:20:36 +0700

    feat: fix create form

--- resources/views/residential-information/create.blade.php
+++ resources/views/residential-information/create.blade.php
@@ -77,53 +77,118 @@
                         </div>
                     
                         <div class="form-group">
-                            <label for="religion_id">ID Agama</label>
-                            <input type="text" class="form-control" id="religion_id" name="religion_id">
+                            <label for="religion_id">Agama</label>
+                            <select class="form-control" id="religion" name="religion">
+                                <option value="1">Islam</option>
+                                <option value="2">Protestan</option>
+                                <option value="3">Katolik</option>
+                                <option value="4">Hindu</option>
+                                <option value="5">Budha</option>
+                                <option value="6">Khonghucu</option>
+                            </select>
                         </div>
                     
                         <div class="form-group">
                             <label for="citizenship">Kewarganegaraan</label>
-                            <input type="text" class="form-control" id="citizenship" name="citizenship">
+                            <select class="form-control" id="citizenship" name="citizenship">
+                                <option value="WNI">WNI</option>
+                                <option value="WNA">WNA</option>
+                            </select>
                         </div>
                     
                         <div class="form-group">
-                            <label for="education_level_id">ID Tingkat Pendidikan</label>
-                            <input type="text" class="form-control" id="education_level_id" name="education_level_id">
+                            <label for="education_level_id">Tingkat Pendidikan</label>
+                            <select class="form-control" id="professions" name="professions">
+                                <option value="1">Tidak/Belum Sekolah</option>
+                                <option value="2">Belum Tamat SD/Sederajat</option>
+                                <option value="3">Tamat SD/Sederajat</option>
+                                <option value="4">SLTP/Sederajat</option>
+                                <option value="5">SLTA/Sederajat</option>
+                                <option value="6">Diploma I/II</option>
+                                <option value="7">Akademi/Diploma III/S.Muda</option>
+                                <option value="8">Diploma IV/Strata I</option>
+                                <option value="9">Strata II</option>
+                                <option value="10">Strata III</option>
+                            </select>
                         </div>
                     
                         <div class="form-group">
                             <label for="blood_type">Golongan Darah</label>
-                            <input type="text" class="form-control" id="blood_type" name="blood_type">
+                            <select class="form-control" id="blood_type" name="blood_type">
+                                <option value="A">A</option>
+                                <option value="B">B</option>
+                                <option value="O">O</option>
+                                <option value="AB">AB</option>
+                            </select>
                         </div>
                     
                         <div class="form-group">
-                            <label for="profession_id">ID Profesi</label>
-                            <input type="text" class="form-control" id="profession_id" name="profession_id">
+                            <label for="profession_id">Profesi</label>
+                            <select class="form-control" id="professions" name="professions">
+                                <option value="1">Tidak Bekerja</option>
+                                <option value="2">Pelajar/Mahasiswa</option>
+                                <option value="3">Swasta</option>
+                                <option value="4">Wiraswasta</option>
+                                <option value="5">Ibu Rumah Tangga</option>
+                                <option value="6">Pensiunan</option>
+                                <option value="7">TNI</option>
+                                <option value="8">Polri</option>
+                                <option value="9">Buruh</option>
+                                <option value="10">Petani</option>
+                                <option value="11">Nelayan</option>
+                                <option value="12">Guru</option>
+                                <option value="13">Dokter</option>
+                                <option value="14">Pengacara</option>
+                            </select>
                         </div>
                     
                         <div class="form-group">
                             <label for="goverment_employees">Pegawai Pemerintah</label>
-                            <input type="text" class="form-control" id="goverment_employees" name="goverment_employees">
+                            <select class="form-control" id="goverment_employees" name="goverment_employees">
+                                <option value="0">Tidak</option>
+                                <option value="1">Ya</option>
+                            </select>
                         </div>
                     
                         <div class="form-group">
-                            <label for="income_range_id">ID Rentang Penghasilan</label>
-                            <input type="text" class="form-control" id="income_range_id" name="income_range_id">
+                            <label for="income_range_id">Rentang Penghasilan</label>
+                            <select class="form-control" id="income_range_id" name="income_range">
+                                <option value="1">Rp. 0 - Rp. 1.000.000</option>
+                                <option value="2">Rp. 1.000.000 - Rp. 2.300.000</option>
+                                <option value="3">Rp. 2.300.000 - Rp. 3.600.000</option>
+                                <option value="4">Rp. 3.600.000 - Rp. 4.900.000</option>
+                                <option value="5">Rp. 4.900.000 - Rp. 6.200.000</option>
+                                <option value="6">Rp. 6.200.000 - Rp. 7.500.000</option>
+                                <option value="7">Rp. 7.500.000 - Rp. 8.800.000</option>
+                                <option value="8">Rp. 8.800.000 - Rp. 10.100.000</option>
+                                <option value="9">Rp. 10.100.000 - Rp. 11.400.000</option>
+                                <option value="10">Rp. 11.400.000 - Rp. 12.700.000</option>
+                                <option value="11">Rp. 14.000.000</option>
+                            </select>
                         </div>
                     
                         <div class="form-group">
-                            <label for="family_id">ID Keluarga</label>
+                            <label for="family_id">Keluarga</label>
                             <input type="text" class="form-control" id="family_id" name="family_id">
                         </div>
                     
                         <div class="form-group">
-                            <label for="family_member_status_id">ID Status Anggota Keluarga</label>
-                            <input type="text" class="form-control" id="family_member_status_id" name="family_member_status_id">
+                            <label for="family_member_status_id">Status Anggota Keluarga</label>
+                            <select class="form-control" id="family_member_status_id" name="family_member_status_id">
+                                <option value="1">Kepala Keluarga</option>
+                                <option value="2">Istri</option>
+                                <option value="3">Anak</option>
+                            </select>
                         </div>
                     
                         <div class="form-group">
-                            <label for="marital_status_id">ID Status Pernikahan</label>
-                            <input type="text" class="form-control" id="marital_status_id" name="marital_status_id">
+                            <label for="marital_status_id">Status Perkawinan</label>
+                            <select class="form-control" id="gender" name="gender">
+                                <option value="1">Belum Kawin</option>
+                                <option value="2">Kawin</option>
+                                <option value="3">Duda/Janda</option>
+                                <option value="4">Cerai</option>
+                            </select>
                         </div>
                     
                         <div class="form-group">
@@ -133,7 +198,10 @@
                     
                         <div class="form-group">
                             <label for="is_archived">Diarsipkan</label>
-                            <input type="checkbox" class="form-control" id="is_archived" name="is_archived">
+                            <select class="form-control" id="is_archived" name="is_archived">
+                                <option value="0">Tidak</option>
+                                <option value="1">Ya</option>
+                            </select>
                         </div>
                     
                         <button type="submit" class="btn btn-primary">Simpan</button>
